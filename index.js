28a1e855083fe6b392c30f885a621ea425abdafb
Ginny Munson <virginia.munson@gmail.com>
2017-11-17 00:47:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, object, [key])
-  delete newObj.key;
+  var newObj = Object.assign({}, object)
+  delete newObj[key];
   return newObj
 }
 
